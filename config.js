1c9d1c020f56f3de417246868089875491525a15
Vishal kumawat <139581408+vk-89@users.noreply.github.com>
2026-08-06 12:12:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-
+// config.js
+// NEVER commit this file to public repositories (add config.js to your .gitignore)
+window.APP_CONFIG = {
+  firebase: {
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  }
+};
